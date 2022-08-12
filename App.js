e2131ba88798387314d3085f06b3ef14f5279d77
Alexis Indick <60584601+aindick@users.noreply.github.com>
2022-08-11 21:01:32 -0400

    adding Login.js and Login.css
    Still trying to figure out how to get the footer to go down the page. Also made an additional bur smal change to the App.js

---
+++ App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import NavbarComp from "./components/NavbarComp";
+import FooterComp from "./components/FooterComp";
+import Login from "./components/Login";
+import { ParseExcel } from "./components/ParseExcel";
+import { Upload } from "./components/Upload";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <NavbarComp />
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />}/>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/parse" element={<ParseExcel />} />
+        <Route path="/upload" element={<Upload />} />
+      </Routes>
+
+      <div>
+        <FooterComp />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
